508101f4a09952de2f92dcce1e576095f3093d26
Andre Nascimento Maia <amaia@uolinc.com>
2022-02-09 21:27:46 -0300

    Fix all API Address

--- libs/api.js
+++ libs/api.js
@@ -9,23 +9,23 @@ export async function queryProducts(query) {
 }
 
 export async function getCart(code) {
-  return fetch(`http://127.0.0.1:5000/carts/${code}`).then((response) =>
+  return fetch(`${API}/carts/${code}`).then((response) =>
     response.json()
   );
 }
 
 export async function updateCart(code, products) {
-  return axios.put(`http://127.0.0.1:5000/carts/${code}`, products);
+  return axios.put(`${API}/carts/${code}`, products);
 }
 
 export async function listOrders() {
-  return axios.get("http://127.0.0.1:5000/orders");
+  return axios.get("${API}/orders");
 }
 
 export async function closeOrder(cart_code) {
-  return axios.post("http://127.0.0.1:5000/orders/", { cart_code: cart_code });
+  return axios.post("${API}/orders/", { cart_code: cart_code });
 }
 
 export async function getOrder(id) {
-  return axios.get(`http://127.0.0.1:5000/orders/${id}`);
+  return axios.get(`${API}/orders/${id}`);
 }
